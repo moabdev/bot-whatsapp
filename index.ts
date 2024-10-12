88a7c7ef335376335f5c4a45b5722a25d80384c9
moabdev <macenamoab3@gmail.com>
2024-10-12 02:56:57 -0300

    All working

--- index.ts
+++ index.ts
@@ -1,33 +1,42 @@
-import express, { Request, Response } from 'express'
-import sendMessage from './sendMessage'
-require('dotenv').config();
+import express, { Request, Response } from "express";
+import sendMessage from "./sendMessage";
+require("dotenv").config();
 
-
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.post('/hook', (req: Request, res: Response) => {
+app.post("/hook", (req: Request, res: Response) => {
   try {
-      const contact = req.body.message.visitor;
-      const message = req.body.message.contents[0].text; 
+    const contact = req.body.message.visitor;
+    const message = req.body.message.contents[0].text;
 
-      if (message.toLowerCase() === 'oi') { 
-          sendMessage(`Olá ${contact.firstName}! Como posso te ajudar? Digite 1 para Menu.`);
-      } else if (message === '1') {
-          sendMessage(`Menu de opções:\n1. Opção A\n2. Opção B\n3. Opção C\nEscolha uma opção (1, 2 ou 3):`);
-      } else if (message === '2') {
-          sendMessage(`Você escolheu a Opção B. Aqui estão mais informações sobre a Opção B.`);
-      } else if (message === '3') {
-          sendMessage(`Você escolheu a Opção C. Aqui estão mais informações sobre a Opção C.`);
-      } else {
-          sendMessage('Me desculpe, não entendi. Digite "oi" para iniciar ou "1" para Menu.');
-      }
+    if (message.toLowerCase() === "oi") {
+      sendMessage(
+        `Olá ${contact.firstName}! Como posso te ajudar? Digite 1 para Menu.`
+      );
+    } else if (message === "1") {
+      sendMessage(
+        `Menu de opções:\n1. Opção A\n2. Opção B\n3. Opção C\nEscolha uma opção (1, 2 ou 3):`
+      );
+    } else if (message === "2") {
+      sendMessage(
+        `Você escolheu a Opção B. Aqui estão mais informações sobre a Opção B.`
+      );
+    } else if (message === "3") {
+      sendMessage(
+        `Você escolheu a Opção C. Aqui estão mais informações sobre a Opção C.`
+      );
+    } else {
+      sendMessage(
+        'Me desculpe, não entendi. Digite "oi" para iniciar ou "1" para Menu.'
+      );
+    }
 
-      res.status(200).end();
+    res.status(200).end();
   } catch (error) {
-      console.error('Erro ao processar a mensagem:', error);
-      res.status(500).send('Erro interno do servidor');
+    console.error("Erro ao processar a mensagem:", error);
+    res.status(500).send("Erro interno do servidor");
   }
 });
 
